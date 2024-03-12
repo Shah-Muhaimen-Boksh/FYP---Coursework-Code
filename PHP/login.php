8d36a491f28e22cec3d126c212ff9f4784a00879
Shah Muhaimen Boksh <s.m.boksh@se21.qmul.ac.uk>
2024-03-12 10:26:48 +0000

    Downloaded example images, fixed login page

--- PHP/login.php
+++ PHP/login.php
@@ -63,7 +63,7 @@
                                     session_start(); // Start a new session
                                     $_SESSION["logged_in"] = "yes"; // Set session variable for checking if the user is logged in
                                     $_SESSION["user_id"] = $user["user_id"]; // Set session variable to store user_id
-                                    header("Location: portfolio_creator.php"); // Redirect to portfolio creator page
+                                    header("Location: portfolio_creator_nosave.php"); // Redirect to portfolio creator page
                                     exit(); // Terminate script execution
                                 }
                                 else{
